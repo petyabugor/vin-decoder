ae7271cb5b3efd4b70fab8085fa6c50e42bbcf6e
Bugor_Petr <102902699+petyabugor@users.noreply.github.com>
2023-01-25 03:43:56 +0200

    ok2

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 
          <Routes basename="/vin-decoder">
             <Route
-               path="/"
+               path="/vin-decoder/"
                element={
                   <Home
                      products={products}

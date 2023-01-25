ad87fa2a591a07e520216247b7fd0ad54cc936ed
Bugor_Petr <102902699+petyabugor@users.noreply.github.com>
2023-01-25 04:02:07 +0200

    ok3

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
       <div className="wrapper">
          <Header />
 
-         <Routes basename="/vin-decoder">
+         <Routes basename="/vin-decoder/">
             <Route
                path="/vin-decoder/"
                element={
